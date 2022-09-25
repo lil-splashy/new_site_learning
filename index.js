6cadb6f54002e657e5fa670cec75eb36399c93b9
lil-splashy <wall.benjamina@gmail.com>
2022-09-25 10:35:32 -0500

    learning functions

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('hello world!');
+// Arrow Function
+const hello = (input) => output
+
+
+
+// Function Expression
+function makeBread(qty) {
+    console.log(arguments);
+    const bread = 'bread'.repeat(qty);
+    console.log(bread)
+    return bread;
+};
+const loaves = makeBread(7);
+
+
+// Named Parameters
+function makeLunch (opts) {
+    const {main, side, drink } = opts;
+}
+
+makeLunch('eggs', 'hashbrowns', 'coffee');
+
+// Rest Parameterse
+function makeDinner (...args) {
+    return 'Dinner includes the following items: ${args.join}'
+}
+
+
+
+// Arrow syntax
+const makeWine = (qty) => {
+    return 'wine'.repeat(qty);
+}
